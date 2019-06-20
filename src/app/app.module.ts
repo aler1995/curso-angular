a36f7275c1402fe991347f75722ee9ab72156853
Aler <aler.alecsandravichus@gmail.com>
2019-06-20 19:23:12 -0300

    Actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { EmpleadoComponent } from './empleado/empleado.component';
 import { HomeComponent } from './home/home.component';
 import { ContactoComponent } from './contacto/contacto.component';
 
+import { ConversorPipe } from './pipes/conversor.pipe';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ContactoComponent } from './contacto/contacto.component';
     FrutaComponent,
     EmpleadoComponent,
     HomeComponent,
-    ContactoComponent
+    ContactoComponent, 
+    ConversorPipe
   ],
   imports: [
     BrowserModule,
